dd45fa5db915dbc406b021f37d9ffb567b46bb3e
sepehr ansaripour <pehrans@gmail.com>
2020-05-20 10:56:40 -0400

    Add clarifying class comment to DB

--- src/main/java/com/sansarip/st8m8/DB.java
+++ src/main/java/com/sansarip/st8m8/DB.java
@@ -1,10 +1,10 @@
 package com.sansarip.st8m8;
 
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
-import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 
+/* Stores the current App and allows the App to be referenced from actions */
 public class DB extends SimpleToolWindowPanel {
     public App app;
     public DB(App app) {
