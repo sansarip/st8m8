a240be54697c459a9265fcefe7ad064a23f567d1
sepehr ansaripour <pehrans@gmail.com>
2020-09-08 10:27:12 -0400

    Add testing comment and empty-vector test for get-map fn

--- test/clj/st8m8/parsley_test.clj
+++ test/clj/st8m8/parsley_test.clj
@@ -1,5 +1,5 @@
 (ns st8m8.parsley-test
-  (:require [clojure.test :refer [is testing]]
+  (:require [clojure.test :refer [is testing deftest]]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :refer [for-all]]
@@ -11,8 +11,15 @@
 
 (defspec test-get-map-returns-first-st8m8-map 20
          (for-all [[_forms-str forms [_ _ expected :as _first-fsm-form]] my-gen/forms-str-gen]
-                  (is (= expected (parsley/get-map forms)))))
+                  (testing "get-map returns the first valid fsm"
+                    (is (= expected (parsley/get-map forms))))))
 
 (defspec test-get-map-returns-nil-when-no-st8m8-map 20
          (for-all [forms (my-gen/forms-gen :fsm? false)]
-                  (is (= nil (parsley/get-map forms)))))
+                  (testing "get-map returns nil"
+                    (is (= nil (parsley/get-map forms))))))
+
+(deftest test-get-map-returns-nil-for-empty-vector
+  (testing "get-map returns nil for an empty vector"
+   (is (= nil (parsley/get-map [])))))
+
